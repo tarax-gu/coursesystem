30f9c798be3b4acad8cd46684bebdd2ff07e65f9
elecfire2022 <99738346+elecfire2022@users.noreply.github.com>
2022-03-31 09:10:35 +0800

    Add files via upload

---
+++ system_main.cpp
@@ -0,0 +1,38 @@
+#include"course_defination.h"
+#include"function1_search.cpp"
+#include"function1_sort.cpp"
+course item[1000];
+int main()
+{
+	int m;
+	cout<<"测试：输入0结束，输入1查找，输入2排序"<<endl;
+	cin >> m;
+	while(m != 0)
+	{
+	   if( m == 1)
+	   {
+	   	cout<<"请输入课程名称:\n"; 
+	   	search();
+		}	
+		else if( m == 2)
+		{
+			
+			
+			int k,total = 0;
+			cout<<"输入排序种类：1名称，2上课地点\n";
+			cin >> k;
+			ifstream in("courseinformation.txt");
+			while(read_course(in, item[total]))total++;
+			in.close();
+			quicksort(item,0,total-1,k);
+			for(int i = 0 ; i < total ; i++)
+			{
+				print_course(cout, item[i]);
+				cout<<endl;
+			}
+		}
+	   cout<<"测试：输入1"<<endl; 	
+	   cin >> m;	
+	} 
+    
+}
